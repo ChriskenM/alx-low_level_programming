25f9f670089f2fe27250041a0689d3642e0502ad
ChriskenM <chriskenmutuma0727@gmail.com>
2023-03-21 02:29:43 -0700

    0-putchar.c commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "0-putchar.h"
+
+/**
+ * main - output _putchar, followed by a new line.
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('U');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
