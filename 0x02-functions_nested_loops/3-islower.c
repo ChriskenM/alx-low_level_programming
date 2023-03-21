619edbc3f329683f51df06f259ba5005b5055e71
ChriskenM <chriskenmutuma0727@gmail.com>
2023-03-21 08:13:12 -0700

    functiond, nested loops mandatory commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
+ * _islower - Checks for lowercase characters.
+ * @c: The  character to be checked.
  *
- * _islower - Checks for lowercase characters
- * c: is the char to be checked
- * Return: 1 if chaar is lowercaase else return 0
+ * Return: 1 if character is lowercaase, else return 0.
  */
 
 int _islower(int c)
