1b798c8d382eb5151dbea1b2aac3e481001f1464
ChriskenM <chriskenmutuma0727@gmail.com>
2023-03-21 07:15:29 -0700

    functiond, nested loops mandatory commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - add two integers
+ * @a: added to b
+ * @b: added to a
+ * Return: value of a+b
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
