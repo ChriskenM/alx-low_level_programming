717380c17ae51d3a7cd842ebef362d5920cda2bf
ChriskenM <chriskenmutuma0727@gmail.com>
2023-04-03 11:38:20 -0700

    pointers corrections

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -12,13 +12,11 @@
 char *_memset(char *s, char b, unsigned int n)
 {
 
-        unsigned int i;
-	char *buffer = s, value = b;
+	unsigned int i;
+	unsigned char *memory = s, value = c;
 
 	for (i = 0; i < n; i++)
+		memory[i] = value;
 
-		buffer[i] = value;
-
-
-	return (buffer);
+	return (memory);
 }
