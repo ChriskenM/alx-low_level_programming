14f380261421f1c7cefed7f95160bbc2ac450697
ChriskenM <chriskenmutuma0727@gmail.com>
2023-04-05 06:06:59 -0700

    Recursion

--- 0x08-recursion/_putchar.c
+++ 0x08-recursion/_putchar.c
@@ -11,3 +11,16 @@ int _putchar(char c)
 {
 	return (write(1, &c, 1));
 }
+
+void _puts_recursion(char *);
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	_puts_recursion("First, solve the problem. Then, write the code");
+	return (0);
+}
