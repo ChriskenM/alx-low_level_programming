887e8a78fbd10b540bd3bbdfeb5fbf64ce83a273
ChriskenM <chriskenmutuma0727@gmail.com>
2023-04-09 10:50:49 -0700

    argc, argv corrections

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -15,7 +15,7 @@
 int main(int argc, char *argv[])
 {
 
-	if (argc > 1)
+	if (argc == 1)
 	{
 
 		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
@@ -25,6 +25,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
+
 	return (0);
 }
 
