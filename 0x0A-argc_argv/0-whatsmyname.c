c2fb719013b5fd88fe8a80ecbbf233e4ccf9906e
ChriskenM <chriskenmutuma0727@gmail.com>
2023-04-09 09:49:36 -0700

    argc, argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,12 +3,17 @@
 
 /**
  *main - program prints its name
+ *@argc: argument index
+ *@argv: a vector of strings
  *
  *Return: Always 0 (successful)
  */
 
 int main(int argc _attribute_((unused)), char *argv[])
 {
+	(void)argc;
+
 	printf("%s\n", argv[0]);
+
 	return (0);
 }
