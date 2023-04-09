abb09cbf9d848d2a0325c0172cb7fdc301f2e161
ChriskenM <chriskenmutuma0727@gmail.com>
2023-04-09 10:02:18 -0700

    argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
  *main - prints number of passed
+ *@argc: argument counter
+ *@argv: a vector of strings
  *
  *Return: Always 0 (successful)
  */
 
 int main(int argc, char *argv[])
 {
+	(void)argv;
+
 	printf("%d\n", argc);
+
 	return (0);
 }
 
