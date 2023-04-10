c04afb4e4afa6646e484d6c6d4901733df8f3fc3
ChriskenM <chriskenmutuma0727@gmail.com>
2023-04-10 11:09:41 -0700

    static lib correction #0

--- 0x09-static_libraries/1-memcpy.c
+++ 0x09-static_libraries/1-memcpy.c
@@ -1,1 +1,21 @@
-char *_memcpy(char *dest, char *src, unsigned int n);
+#include "main.h"
+
+/**
+ * _memcpy -  function copies n bytes from memory area src to memory area dest
+ * @dest: destination string
+ * @src: source string to copy from
+ * @n: number of bytes
+ *
+ * Return: pointer to @dest destination buffer.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
--- 0x09-static_libraries/2-strchr.c
+++ 0x09-static_libraries/2-strchr.c
@@ -1,1 +1,23 @@
-char *_strchr(char *s, char c);
+#include "main.h"
+
+/**
+ *_strchr - Entry point
+ * or NULL if the character is not found.
+ *@s: the string to be searched
+ *@c: the character to look for
+ *
+ *Return: Always 0 (success)
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (s + i);
+
+	}
+	return ('\0');
+}
