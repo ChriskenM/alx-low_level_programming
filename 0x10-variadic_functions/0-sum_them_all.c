72a37400118dc7183a183174ec67d8e9c0e6f352
ChriskenM <chriskenmutuma0727@gmail.com>
2023-04-21 08:28:43 -0700

    1st variadic function commit beyond deadline

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+
+/**
+ *sum_them_all - sums up ints
+ *@n: int to sum
+ *
+ *Return: 0 successful
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+	unsigned int i = 0;
+
+	if (n != 0)
+	{
+		va_start(args, n);
+
+		while (i < n)
+		{
+			sum += va_arg(args, int);
+			i++;
+		}
+
+		va_end(args);
+		return (sum);
+	}
+	return (0);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ *print_strings - prints strings
+ *@n: number of strings passsed on function
+ *@separator: separates elements string to be printed btwn strings
+ *
+ *Return: NULL
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	char *s;
+	unsigned int i = 0;
+
+	if (n > 0)
+	{
+		va_start(args, n);
+
+		while (i < n)
+		{
+			s = va_arg(args, char *);
+			if (s == NULL)
+				printf("%s", "(nil)");
+			else
+				printf("%s", separator);
+
+			if (i != n - 1 && separator != NULL)
+				printf("%s", separator);
+			i++;
+		}
+		va_end(args);
+	}
+	printf("\n");
+}
+
