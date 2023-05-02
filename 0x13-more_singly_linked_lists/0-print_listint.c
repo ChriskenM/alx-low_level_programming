401214bbfa1b1f51820000cfbfa6d78df408b78e
ChriskenM <chriskenmutuma0727@gmail.com>
2023-05-02 02:31:18 -0700

    debbuging 10-delete_nodeint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,14 +12,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t j = 0;
 
-	if (h != NULL)
+	while (h != NULL)
 	{
-		while (h)
-		{
-			printf("%d\n", h->n);
-			h = h->next;
-			j++;
-		}
+		printf("%d\n", h->n);
+		h = h->next;
+		j++;
 	}
 	return (j);
 }
