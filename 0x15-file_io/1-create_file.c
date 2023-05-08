00d075ccb55f8c16609b25a02638d048f18ffbd7
ChriskenM <chriskenmutuma0727@gmail.com>
2023-05-08 04:03:12 -0700

    file_io

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+ *create_file - creates a filr
+ *@filename: name of the source file
+ *@text_content - string to write into filename
+ *
+ *Return: 1 on success, -1 on failure
+ *(file can not be created, file can not be written, write “fails”, etc…)
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fn;
+
+	if (!filename)
+		return (-1);
+
+	fn = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fn == -1)
+		return (-1);
+
+	if (text_content)
+		write(fn, text_content, _strlen(text_content));
+
+	close (fn);
+	return (1);
+}
+
+/**
+ *_strlen - returns length of string
+ *@s: string
+ *
+ *Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+
+	return (i);
+}
---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,68 @@
+#include "main.h"
+
+/**
+ *main - entry
+ *@argc: argument counter
+ *@argv: argument vector
+ *
+ *Return: nothing
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	copy_file(argv[1], argv[2]);
+	exit(0);
+}
+
+/**
+ *copy_file - copies to file_to
+ *@src: source file
+ *@dest: destination file
+ *
+ *Return: nothing
+ */
+
+void copy_file(const char *src, const char *dest)
+{
+	int ofn, tfn, readn;
+	char buff[1024];
+
+	ofn = open(src, O_RDONLY);
+	if (!src || ofn == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", src);
+		exit(98);
+	}
+
+	tfn = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((readn = read(ofn, buff, 1024)) > 0)
+	{
+		if (write(tfn, buff, readn) != readn || tfn == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: can't write to %s\n", dest);
+			exit(99);
+		}
+	}
+	if (readn == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't read from file %s\n", src);
+		exit(98);
+	}
+	if (close(ofn) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fn %d\n", ofn);
+		exit(100);
+	}
+	if (close(tfn) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't close fn %d\n", tfn);
+		exit(100);
+	}
+}
+
+
