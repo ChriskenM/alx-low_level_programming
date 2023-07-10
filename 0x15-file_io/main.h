c35c5f925983b7da2c88b698667c9aa6f8c74544
ChriskenM <chriskenmutuma0727@gmail.com>
2023-07-10 10:39:42 -0700

    corrections

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -16,5 +16,17 @@ int main(int argc, char **argv);
 void copy_file(const char *src, const char *dest);
 int _strlen(char *s);
 
+/*elf_header.c*/
+#include <elf.h>
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif
