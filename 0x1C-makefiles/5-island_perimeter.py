19a0156f94971ffea00d26aeb09b951276420721
ChriskenM <chriskenmutuma0727@gmail.com>
2023-12-20 00:59:24 -0800

    basic and mandatory files

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+script to get island's perimeter
+"""
+
+def island_perimeter(grid):
+    """ Function returns the perimeter of island in grid """
+    perimeter = 0
+
+    rowsno = len(grid)
+
+    if grid != []:
+        columnsno = len(grid[0])
+
+    for a in range(rowsno):
+        for b in range(columnsno):
+            if grid[a][b] == 1:
+                if (a - 1) == -1 or grid[a - 1][b] == 0:
+                    perimeter += 1
+                if (a + 1) == rowsno or grid[a + 1][b] == 0:
+                    perimeter += 1
+                if (b - 1) == -1 or grid[a][b - 1] == 0:
+                    perimeter += 1
+                if (b + 1) == columnsno or grid[a][b + 1] == 0:
+                    perimeter += 1
+
+    return perimeter
---
+++ 0x1C-makefiles/README.md
@@ -0,0 +1,25 @@
+# **0x1C. C - Makefiles**
+
++ What are make, Makefiles
++ When, why and how to use Makefiles
++ What are rules and how to set and use them
++ What are explicit and implicit rules
++ What are the most common / useful rules
++ What are variables and how to set and use them 
+
+# **Requirements**
+
++ OS: Ubuntu 20.04 LTS
++ Version of gcc: 9.3.0
++ Version of make: GNU Make 4.2.1
++ All your files should end with a new line
+
++ **going to use these files. We want to compile these only:**
+
++ a.out
+
++ m.h
+
++ main.c
+
++ school.c
